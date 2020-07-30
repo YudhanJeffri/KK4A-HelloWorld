736b15e2f342842b1d81e18550a428ee31663e2a
YudhanJeffri <yudhanjeffri168@gmail.com>
2020-07-30 13:10:36 +0700

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+
+/*
+Nama  : Yudhan Jeffri Djuniartha
+Kelas : XI RPL 5
+No    : 31
+ */
+
+class Perkenalan(){
+    fun Nama(){
+        println("Nama Saya Yudhan Jeffri Djuniartha")
+    }
+    fun Kelas(){
+        println("Kelas XI RPL 5")
+    }
+    fun NoAbsen(){
+        println("No Absen 31")
+    }
+}
+fun main(){
+    println("Hello It's me! ")
+    val kenalan = Perkenalan()
+    kenalan.Nama()
+    kenalan.Kelas()
+    kenalan.NoAbsen()
+}
